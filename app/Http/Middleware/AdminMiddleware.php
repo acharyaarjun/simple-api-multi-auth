b68afea3bc773d8acd11198e5502657aaa246069
Arjun Acharya <legendarjun333@gmail.com>
2023-02-17 16:15:33 +0545

    multiple authenticaiton using passport and middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        $user = Auth::user();
+        if ($user->is_admin == 'Y') {
+            return $next($request);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => "Unauthenticated",
+                'date' => Carbon::now()->toJSON(),
+                'data' => null
+            ], 401);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    // yeha vitra user ko route haru lekni
+});
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::middleware('admin')->group(function () {
+        // yeha vitra admin ko route har lekni
+    });
+});
